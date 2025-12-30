0e5436b3729c86cda75657dddcc7153e86dd8976
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-12-30 11:12:27 -0500

    implementacion de cola

--- src/main/java/org/algoritmos/structures/linear/Queue.java
+++ src/main/java/org/algoritmos/structures/linear/Queue.java
@@ -3,5 +3,40 @@
  */
 package org.algoritmos.structures.linear;
 
-public class Queue {
+
+public class Queue<T extends Comparable<T>> {
+
+    private final SingleLinkedList<T> internalList;
+
+    public Queue() {
+        this.internalList = new SingleLinkedList<>();
+    }
+
+
+    public void enqueue(T element) {
+        this.internalList.pushBack(element);
+    }
+
+    public T dequeue() {
+        return this.internalList.popFront();
+    }
+
+    public T peek() {
+        return this.internalList.topFront();
+    }
+
+    public int size() {
+        return this.internalList.size();
+    }
+
+    public boolean isEmpty() {
+        return this.internalList.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return this.internalList.toString();
+    }
+
+
 }
