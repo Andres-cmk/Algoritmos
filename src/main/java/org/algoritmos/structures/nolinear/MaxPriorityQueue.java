f78893592463feb2f6ebcbeb0f1f1a2245c69376
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2026-01-31 23:19:46 -0500

    cola de prioridad maxima imple

--- src/main/java/org/algoritmos/structures/nolinear/MaxPriorityQueue.java
+++ src/main/java/org/algoritmos/structures/nolinear/MaxPriorityQueue.java
@@ -1,4 +1,55 @@
-package org.algoritmos.structures.nolinear;
+    /**
+     * @autor Andres Ramirez (Andres-cmk)
+     *
+     */
+    package org.algoritmos.structures.nolinear;
 
-public class MaxPriorityQueue {
-}
+    import org.algoritmos.structures.linear.List;
+
+    public class MaxPriorityQueue<T extends Comparable<T>> extends Heap<T> {
+
+        public MaxPriorityQueue(List<T> list) {
+            super(list);
+            buildMaxHeap();
+        }
+
+        public int sizeMaxPriorityQueue() {
+            return size();
+        }
+
+        public boolean isEmptyMaxPriorityQueue() {
+            return isEmpty();
+        }
+
+        @Override
+        public T peek() {
+            return super.peek();
+        }
+
+        public void insert(T item) {
+            insertMaxHeap(item);
+        }
+
+        public T removeMax() {
+            return extractMaxElement();
+        }
+
+        public T extractElement(T key){
+            return extractMaxElement();
+        }
+
+
+        @Override
+        public String toString() {
+            return heap.toString();
+        }
+
+
+        public static void main(String[] args) {
+            List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            MaxPriorityQueue<Integer> max = new MaxPriorityQueue<>(list);
+            System.out.println(max);
+
+        }
+
+    }
