71dcd48c5521f945903bc00c4875e84b238a4512
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2026-01-31 23:22:23 -0500

    cola de prioridad minima

--- src/main/java/org/algoritmos/structures/nolinear/MinPriorityQueue.java
+++ src/main/java/org/algoritmos/structures/nolinear/MinPriorityQueue.java
@@ -1,4 +1,51 @@
+/**
+ * @autor Andres Ramirez (Andres-cmk)
+ *
+ */
 package org.algoritmos.structures.nolinear;
 
-public class MinPriorityQueue {
+import org.algoritmos.structures.linear.List;
+
+public class MinPriorityQueue<T extends Comparable<T>> extends Heap<T> {
+
+    public MinPriorityQueue(List<T> list) {
+        super(list);
+        buildMinHeap();
+    }
+
+    public int sizeMinPriorityQueue() {
+        return size();
+    }
+
+    public boolean isEmptyMinPriorityQueue() {
+        return isEmpty();
+    }
+
+    @Override
+    public T peek() {
+        return super.peek();
+    }
+
+    public void insert(T item) {
+       insertMinHeap(item);
+    }
+
+    public T removeMin() {
+        return extractMinElement();
+    }
+
+    public T extractElement(T value) {
+        return extracHeap(value);
+    }
+
+    @Override
+    public String toString() {
+        return heap.toString();
+    }
+    public static void main(String[] args) {
+        List<Integer> list = List.of(5,2,1,7,9,4,1);
+        MinPriorityQueue<Integer> max = new MinPriorityQueue<>(list);
+        System.out.println(max);
+    }
+
 }
