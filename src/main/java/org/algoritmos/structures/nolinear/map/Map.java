a63f726c2ee591e6b9f9d332705da9073050435f
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2026-02-08 20:27:52 -0500

    interfaz para estructuras Hash

--- src/main/java/org/algoritmos/structures/nolinear/map/Map.java
+++ src/main/java/org/algoritmos/structures/nolinear/map/Map.java
@@ -1,4 +1,16 @@
+/**
+ * @autor Andres Ramirez (Andres-cmk)
+ */
 package org.algoritmos.structures.nolinear.map;
 
-public interface Map {
+public interface Map<K, V> {
+    void put(K key, V value);
+    int hash(K key);
+    V get(K key);
+    void remove(K key);
+    void clear();
+    int size();
+    boolean isEmpty();
+    boolean containsKey(K key);
+    boolean containsValue(V value);
 }
