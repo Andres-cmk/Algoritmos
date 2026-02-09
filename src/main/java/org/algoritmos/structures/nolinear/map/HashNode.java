387cc2f2a16bf0fccf6e08431645620cc435877d
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2026-02-08 20:27:24 -0500

    Nodo Hash

--- src/main/java/org/algoritmos/structures/nolinear/map/HashNode.java
+++ src/main/java/org/algoritmos/structures/nolinear/map/HashNode.java
@@ -1,4 +1,30 @@
 package org.algoritmos.structures.nolinear.map;
 
-public class HashNode {
+import java.util.Objects;
+
+public class HashNode<K, V> {
+
+    private final K key;
+    private V value;
+    private HashNode<K, V> next;
+
+    public HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+
+    // --- Getters y Setters ---
+
+    public K getKey() {return key;}
+    public V getValue() {return value;}
+    public void setValue(V value) {this.value = value;}
+    public HashNode<K, V> getNext() {return next;}
+    public void setNext(HashNode<K, V> next) {this.next = next;}
+
+
+    @Override
+    public String toString() {
+        return key + " = " + value;
+    }
 }
